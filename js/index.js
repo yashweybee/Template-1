1420b4e07122054c19f31ef6280fc333eb7f16b1
yashweybee <yashparmar@weybee.in>
2023-08-28 18:49:22 +0530

    progress bad and update section completed

--- js/index.js
+++ js/index.js
@@ -82,3 +82,14 @@ document.addEventListener("DOMContentLoaded", () => {
     counter("count3", 0, 90, 1000);
     counter("count4", 0, 78, 1000);
 });
+
+function progressBarScroll() {
+    let winScroll = document.body.scrollTop || document.documentElement.scrollTop,
+        height = document.documentElement.scrollHeight - document.documentElement.clientHeight,
+        scrolled = (winScroll / height) * 100;
+    document.getElementById("progressBar").style.width = scrolled + "%";
+}
+
+window.onscroll = function () {
+    progressBarScroll();
+};
